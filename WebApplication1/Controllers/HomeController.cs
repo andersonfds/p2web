d8c2f68187161d82d62b5d5c6bee01c47da4be29
andersonfds <andersonlivegja.as@gmail.com>
2021-06-04 22:19:58 -0300

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(string name)
+        {
+            HttpCookie cookie = new HttpCookie("user", "teste");
+            cookie.Expires = DateTime.Now.AddDays(40);
+            Response.Cookies.Add(cookie);
+            ViewBag.Message = cookie.Value;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            var cookie = Request.Cookies["user"];
+            ViewBag.Message = cookie.Value;
+
+            return View();
+        }
+    }
+}
